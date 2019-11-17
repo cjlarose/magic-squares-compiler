b484fcb84525ff5841777dd912bb3b4c27d9294f
Chris LaRose <cjlarose@gmail.com>
2019-11-16 20:50:09 -0800

    Extract matrixType

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,6 +6,7 @@ module Lib
 
 import LLVM.AST
 import qualified LLVM.AST as AST
+import LLVM.AST.Type (i32)
 import qualified LLVM.AST.Linkage as Linkage
 import LLVM.AST.Global
 import LLVM.Context
@@ -14,17 +15,20 @@ import LLVM.Module
 import Control.Monad.Except
 import qualified Data.ByteString.Char8 as BS
 
+matrixType :: Int -> Type
+matrixType n = AST.ArrayType
+  { nArrayElements = fromIntegral n
+  , elementType = AST.ArrayType
+    { nArrayElements = fromIntegral n
+    , elementType = i32
+    }
+  }
+
 defGlobalSquare :: Int -> Definition
 defGlobalSquare n = GlobalDefinition globalVariableDefaults
   { name = Name "square"
   , linkage = Linkage.Private
-  , LLVM.AST.Global.type' = AST.ArrayType
-    { nArrayElements = fromIntegral n
-    , elementType = AST.ArrayType
-      { nArrayElements = fromIntegral n
-      , elementType = AST.IntegerType 32
-      }
-    }
+  , LLVM.AST.Global.type' = matrixType n
   }
 
 enumerationModule :: Int -> AST.Module
