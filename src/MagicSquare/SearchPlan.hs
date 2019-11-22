c70f232340cabe0a34d6cc6f9cc0efa55e60c539
Chris LaRose <cjlarose@gmail.com>
2019-11-22 07:44:24 -0800

    Perform topological sort on matrix position dependency graph

--- src/MagicSquare/SearchPlan.hs
+++ src/MagicSquare/SearchPlan.hs
@@ -8,7 +8,9 @@ module MagicSquare.SearchPlan
 import Data.Matrix (Matrix, fromLists, rref, nrows, ncols, (!))
 import Data.Ratio ((%), numerator, denominator)
 import Data.Maybe (isNothing, fromJust)
-import Data.List (find)
+import Data.List (find, maximumBy, partition, foldl')
+import Data.Ord (comparing)
+import qualified Data.Set as Set
 
 import MagicSquare.AST (ComputedResultTerm(..), MatrixPosition(..))
 
@@ -89,6 +91,50 @@ matrixPositions n = positions <$> reduced
         positionAtCol j = case findPivot j of
                             Just p  -> InducedPosition (originalPosition j) $ equationForPivot p
                             Nothing -> FreePosition . originalPosition $ j
+
+topoSort :: [MatrixPosition] -> [MatrixPosition]
+topoSort xs = f [] xs
+  where
+    f :: [[MatrixPosition]] -> [MatrixPosition] -> [MatrixPosition]
+    f acc remaining
+      | null remaining = concat . reverse $ acc
+      | otherwise      = f (bestChoice : acc) leftover
+      where
+        isFree :: MatrixPosition -> Bool
+        isFree (FreePosition _) = True
+        isFree _ = False
+
+        remainingFreePositions :: [MatrixPosition]
+        remainingInducedPositions :: [MatrixPosition]
+        (remainingFreePositions, remainingInducedPositions) = partition isFree remaining
+
+        computedFreePositions :: Set.Set (Int, Int)
+        computedFreePositions = Set.fromList . map coord . filter isFree . concat $ acc
+
+        dependencies :: MatrixPosition -> Set.Set (Int, Int)
+        dependencies (InducedPosition _ terms) =
+          foldl' (\acc term -> case term of
+                                 PositionWithCoefficientTerm _ p -> Set.insert p acc
+                                 _ -> acc) Set.empty terms
+
+        satisfiedBy :: Set.Set (Int, Int) -> MatrixPosition -> Bool
+        satisfiedBy computed inducedPosition = Set.isSubsetOf (dependencies inducedPosition) computed
+
+        satisfiableDependents :: MatrixPosition -> [MatrixPosition]
+        satisfiableDependents (FreePosition p) = filter (satisfiedBy (Set.insert p computedFreePositions)) remainingInducedPositions
+
+        choices :: [[MatrixPosition]]
+        choices = map (\p -> p : satisfiableDependents p) remainingFreePositions
+
+        bestChoice :: [MatrixPosition]
+        bestChoice = maximumBy (comparing length) choices
+
+        coord :: MatrixPosition -> (Int, Int)
+        coord (FreePosition c) = c
+        coord (InducedPosition c _) = c
+
+        leftover :: [MatrixPosition]
+        leftover = filter (\mp -> not $ (coord mp) `elem` (map coord bestChoice)) remaining
 
 searchPlan :: Int -> [MatrixPosition]
 searchPlan 4 = [ FreePosition (0, 0) -- a
