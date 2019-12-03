13ad76b0c41d433cae4d2cb14773853e00a50437
Chris LaRose <cjlarose@gmail.com>
2019-12-03 08:07:43 -0800

    Add "plan" command which prints out the plan for a given order n

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import System.IO (hPutStrLn, stderr)
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
 
 import qualified Data.ByteString.Char8 as BS
 import qualified LLVM.AST as AST
@@ -9,6 +11,7 @@ import LLVM.Module (moduleLLVMAssembly, withModuleFromAST)
 
 import MagicSquare.CodeGen.ModuleGen (enumerationModule)
 import MagicSquare.SearchPlan (searchPlan)
+import MagicSquare.SearchPlan.Serialize (serialize)
 
 toLLVM :: AST.Module -> IO ()
 toLLVM mod = withContext $ \ctx -> do
@@ -18,5 +21,26 @@ toLLVM mod = withContext $ \ctx -> do
 printModule :: Int -> IO ()
 printModule n = either (hPutStrLn stderr) (toLLVM . enumerationModule n) $ searchPlan n
 
+plan :: [String] -> IO ()
+plan (arg0:_) = do
+  let n = read arg0 :: Int
+  let planResult = searchPlan n
+  case planResult of
+    Left err -> do
+      hPutStrLn stderr err
+      exitFailure
+    Right plan -> do
+      putStr . serialize n $ plan
+plan _ = do
+  hPutStrLn stderr "usage: magic-squares-compiler-exe plan n"
+  exitFailure
+
 main :: IO ()
-main = printModule 4
+main = do
+  args <- getArgs
+  case args of
+    [] -> printModule 4
+    ("plan":restArgs) -> plan restArgs
+    (command:_) -> do
+      hPutStrLn stderr $ "Unknown command " ++ command
+      exitFailure
